e022389491abf0bc9cb48a0f2ed4fe53589465c6
fennyflop <fennyflop@gmail.com>
2022-07-09 12:24:49 +0300

    welcoming component media query

--- components/welcoming-component/welcoming-component.tsx
+++ components/welcoming-component/welcoming-component.tsx
@@ -19,6 +19,10 @@ const useStyles = createStyles((theme) => ({
     justifyContent: 'space-between',
     paddingTop: theme.spacing.xl * 4,
     paddingBottom: theme.spacing.xl * 4,
+
+    [theme.fn.smallerThan('md')]: {
+      justifyContent: 'center'
+    },
   },
 
   content: {
@@ -28,6 +32,9 @@ const useStyles = createStyles((theme) => ({
     [theme.fn.smallerThan('md')]: {
       maxWidth: '100%',
       marginRight: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
     },
   },
 
@@ -38,14 +45,42 @@ const useStyles = createStyles((theme) => ({
     lineHeight: 1.2,
     fontWeight: 900,
 
+    [theme.fn.smallerThan('md')]: {
+      textAlign: 'center',
+    },
     [theme.fn.smallerThan('xs')]: {
       fontSize: 28,
+    },
+  },
+
+  subtitle: {
+    [theme.fn.smallerThan('md')]: {
+      textAlign: 'center',
+    },
+    [theme.fn.smallerThan('xs')]: {
+      maxWidth: 430,  
     },
   },
 
   control: {
     [theme.fn.smallerThan('xs')]: {
       flex: 1,
+    },
+  },
+
+  list: {
+    [theme.fn.smallerThan('md')]: {
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      maxWidth: 430,
+    },
+  },
+
+  buttons: {
+    [theme.fn.smallerThan('md')]: {
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      // maxWidth: 430,
     },
   },
 
@@ -78,7 +113,7 @@ export function WelcomingComponent() {
             <Title className={classes.title}>
               Sanction Proof <span className={classes.highlight}>Your</span> <br /> Online Payments
             </Title>
-            <Text color="dimmed" mt="md">
+            <Text color="dimmed" mt="md" className={classes.subtitle}>
             Our virtual payment cards allow you to have unlocked shopping worldwide.
             </Text>
 
@@ -91,6 +126,7 @@ export function WelcomingComponent() {
                   <Check size={12} />
                 </ThemeIcon>
               }
+              className={classes.list}
             >
               <List.Item>
                 <b>One-Time Use Cards</b> – for secure one-time payments online.
@@ -100,7 +136,7 @@ export function WelcomingComponent() {
               </List.Item>
             </List>
 
-            <Group mt={30}>
+            <Group mt={30} className={classes.buttons}>
               <Button size="md" className={classes.control}>
                 Get started
               </Button>

56009bbdd45cb5121d9f025be4dc99d9964551bb
fennyflop <fennyflop@gmail.com>
2022-07-10 20:59:41 +0300

    added team members component

--- components/team-members/team-members.tsx
+++ components/team-members/team-members.tsx
@@ -17,10 +17,10 @@ const TeamMember: FC<ITeamMember> = ({name, position, description, avatar, email
                 <img className={styles.image} src={avatar} alt={name} />
                 <Text size="md" weight={700} className={styles.name}>{name}</Text>
                 <Text className={styles.position} size={'md'} weight={700}>{position}</Text>
-                {/* <Text size="xs" className={styles.email} color="gray" weight={600}>{email}</Text> */}
             </div>
+            <Text size="xs" className={styles.email} color="gray" weight={600}>{email}</Text>
 
-            <Text className={styles.description} color="gray" weight={600}>{description}</Text>
+            <Text className={styles.description} color="black" weight={600}>{description}</Text>
         </li>
     );
 }
@@ -30,9 +30,9 @@ const TeamMembers = () => {
         <section className={styles.container}>
             <Text component='h2' size="xl" className={styles.team} weight={700}>Meet the <span className={styles.highlight}>Team</span></Text>
             <ul className={styles.list}>
-                <TeamMember email={'alex@sanctioncards.com'} name={'Alex Semochkin'} position={'CTO'} description={'Responsible for developing.'} avatar={'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'} />
-                <TeamMember email={'alex@sanctioncards.com'} name={'Maxim Lyash'} position={'CEO'} description={'Responsible for legal issues.'} avatar={'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'} />
-                <TeamMember email={'alex@sanctioncards.com'} name={'Oleg Tinkoff'} position={'COO'} description={'Responsible for marketing.'} avatar={'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'} />
+                <TeamMember email={'alex@sanctioncards.com'} name={'Alex Semochkin'} position={'CTO'} description={'Wants to create a better world to live in. Works as a developer.'} avatar={'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'} />
+                <TeamMember email={'alex@sanctioncards.com'} name={'Alex Semochkin'} position={'CTO'} description={'Wants to create a better world to live in. Works as a developer.'} avatar={'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'} />
+                <TeamMember email={'alex@sanctioncards.com'} name={'Alex Semochkin'} position={'CTO'} description={'Wants to create a better world to live in. Works as a developer.'} avatar={'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'} />
             </ul>
 
         </section>
